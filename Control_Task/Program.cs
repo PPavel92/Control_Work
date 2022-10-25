087b06ed3129fa761e838e82208defd9f6e59e82
PavelProskuryakovp <PPavel@mail.ru>
2022-10-25 17:47:06 +0600

    add_method

--- Control_Task/Program.cs
+++ Control_Task/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк,
+//  длина которых меньше либо равна 3 символа. 
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
+
+
+string[] array1 = new string[5] {"Russia", "cat", "1,5", "Kazan", "-2"};
+string[] array2 = new string[array1.Length];
+
+void SecondArrayWithIF(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+
+SecondArrayWithIF(array1, array2);
